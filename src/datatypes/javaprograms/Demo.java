477dcbba07427b70d986cb526e7f66d5dcf6c5cd
josetellis-2024 <awsjoetellis@gmail.com>
2024-09-17 09:54:10 +0530

    java class created

---
+++ src/datatypes/javaprograms/Demo.java
@@ -0,0 +1,23 @@
+package datatypes.javaprograms;
+
+public class Demo 
+{
+	int a;
+	String name;
+	public Demo(int a, String name) {
+		super();
+		this.a = a;
+		this.name = name;
+	}
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+
+	}
+	@Override
+	public String toString() {
+		return "Demo [a=" + a + ", name=" + name + "]";
+	}
+
+}
---
+++ src/datatypes/javaprograms/Main.java
@@ -0,0 +1,21 @@
+package datatypes.javaprograms;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+	    int num=10;
+	    int i=2;
+	    while(i<num)
+	    {
+	    	if(i%num==0)
+	    	{
+	    		System.out.println(i);
+	    	}
+	    	i++;
+	    }
+	    
+
+	}
+
+}
